50eb91e0cd2c7d1dab6717709c1cf47bb6da6a84
Mercesletifer47 <art-matv98@yandex.ru>
2021-06-16 20:56:18 +0300

    Commit 1

---
+++ src/ru/geekbrains/java_1_15062021/Main.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.java_1_15062021;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+	// write your code here
+    }
+}
